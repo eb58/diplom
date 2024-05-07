aba65f01948ab6d46393ca85cb9be0138df737d7
Erich Brandl <erich.brandl@gmail.com>
2024-05-07 08:59:10 +0200

    wip

--- js/math.js
+++ js/math.js
@@ -1,17 +1,15 @@
 const { complex, add, subtract, multiply, divide, pi } = { ...math }
 
-const DIM = 3;
-const i = complex(0, 1)
-
 const range = (n) => [...Array(n).keys()];
+const zip = (xs, ys, f) => xs.map((x, i) => f ? f(x, ys[i]) : [x, ys[i]])
+
 const zline = (x, z1, z2) => add(multiply(x, z2), multiply(1 - x, z1))
-const zip = (xs, ys, f) => xs.map((x, i) => f ? f(xs[i], ys[i]) : [xs[i], ys[i]])
 const csqr = (z) => multiply(z, z)
 const v3add = (v1, v2) => zip(v1, v2, (x, y) => x + y)
 const transpose = m => m[0].map((x, i) => m.map(x => x[i]))
 
-lineIntegral = (z1, z2, F) => {
-    const N = 10
+const lineIntegral = (z1, z2, F) => {
+    const N = 20
     const dx = 1.0 / N
     let x = 0
     let sum = complex(0, 0)
@@ -24,7 +22,7 @@ lineIntegral = (z1, z2, F) => {
     return sum
 }
 
-circleGrid = (r1, r2, w1, w2) => {  // Parameterbereich: Kreisring um 0
+const circleGrid = (r1, r2, w1, w2) => {  // Parameterbereich: Kreisring um 0
     const GRIDSIZE_R = 30
     const GRIDSIZE_W = 70
 
@@ -48,6 +46,7 @@ const evalWeierstrass = (grid, f, g) => {
         return multiply(ff, subtract(1, csqr(gg)))
     }
     const Phi2 = (z) => {
+        const i = complex(0, 1)
         const ff = multiply(0.5, f(z))
         const gg = g(z)
         return multiply(i, multiply(ff, add(1, csqr(gg))));
@@ -60,7 +59,7 @@ const evalWeierstrass = (grid, f, g) => {
         lineIntegral(za, ze, Phi3).re,
     ]
 
-    const data = Array(grid.length).fill(0).map(() => Array(grid[0].length).fill([0, 0, 0]))
+    const data = range(grid.length).map(() => range(grid[0].length).map(x=> [0, 0, 0]))
     for (let i = 0; i < grid.length - 1; i++) { // Integration entlang Kreisbogen
         const gridRow = grid[i], dataX = data[i]
         for (let j = 0; j < gridRow.length - 1; j++) {
